6530fc11f97cf773693c8b3d1ad9d69f37c2467f
David Strube <dstrube1@users.noreply.github.com>
2024-09-03 09:29:23 -0400

    Update Factors.cpp

--- old/Factors.cpp
+++ old/Factors.cpp
@@ -1,26 +1,40 @@
-#include<iostream.h>
+#include <iostream>
+
 int main()
-{                        // main
-int count=0, x, y, fact1, fact2;
-while (count<=100)
-	{                       //count while
-	count++;
-	x=0;
-	y=0;
-	while (x<count)
-		{                      //x while
-		x++;
-		while (y<count)
-			{                     //y while
-			y++;
-			if (x*y==count)
-				{                    //if open
-				fact1=x;
-				fact2=y;
-				cout<< fact1 << " & " << fact2 << "are factors of " << count;
-				}                    //close if
-			 }                    //close y
-		}                     //close x
-	}                      //close count
-return 0;
+{                        					// main
+	const int min = 1;
+	const int max = 100;
+	int count = min, x, y;
+	vector<int> factors {};
+	vector<int> primes {};
+	while (count <= max)
+	{                       				//count while
+		x = 1;
+		while (x <= count) {				//x while
+			y = 1;
+			while (y <= count) {			//y while
+				if (x * y == count && x <= y) {	//if open
+					factors.push_back(x);
+					if (x != y) factors.push_back(y);
+				}                    		//close if
+				y++;
+			}                    			//close y while
+			x++;
+		}                     				//close x while
+		cout << "factors of " << count << ": ";
+		for(auto f : factors) {
+			cout << f << ", ";
+		}
+		cout << endl;
+		if (factors.size() == 2) primes.push_back(count);
+		factors.clear();
+		count++;
+	}                      					//close count while
+	cout << "primes (from " << min << " to " << max << "): ";
+	for(auto p : primes) {
+		cout << p << ", ";
+	}
+	cout << endl;
+
+	return 0;
 }
