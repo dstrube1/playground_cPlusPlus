fcb78facd86356c096af741728ecd5c2c7f3a126
David Strube <dstrube1@users.noreply.github.com>
2024-09-30 12:22:47 -0400

    Update main.cpp
    Pointers and references

--- udemy/main.cpp
+++ udemy/main.cpp
@@ -56,6 +56,10 @@ double global_d;
 inline int add(int a, int b) { return a + b; }
 //unsigned long long factorial(unsigned long long n);
 //double a_penny_doubled_everyday(int days, double total_amount );
+//void double_target(int *ptr);
+//int *largest(int *ptr1, int *ptr2);
+//int *make_array(size_t size, int initial_value = 0); //default parameter
+
 
 int main()
 {
@@ -626,6 +630,66 @@ void old_tests(){
 
 */
 
+	//Pointers and references
+	/*
+ 	int i1 = 1;
+	int i2 = 2;
+	int *ptr1 {&i1};
+	int *ptr2 {&i2};
+	//two ways to swap two pointers without using a 3rd variable:
+	//1:
+	*ptr1 = *ptr1 + *ptr2;
+	*ptr2 = *ptr1 - *ptr2;
+	*ptr1 = *ptr1 - *ptr2;
+	//2:
+	std::swap(*ptr1, *ptr2);
+
+	//pointer to a constant: cannot change the value of the thing pointed to, but can change what the pointer points to
+	const int *ptr3 {&i1};
+	//*ptr3 = 3; //compiler error
+	ptr3 = &i2; //OK
+	//const pointer:
+	int *const ptr4 {&i1};
+	//ptr4 = &i2; //compiler error
+	*ptr4 = 3; //OK
+	//const pointer to a constant:
+	const int *const ptr5 {&i1};
+	//ptr5 = &i2; //compiler error
+	//*ptr5 = 3; //compiler error
+	
+	cout << "Before doubling: " << i1 << endl;
+	double_target(&i1); // pass in the address of the target, i.e. pass by reference
+	cout << "After doubling: " << i1 << endl;
+
+	int *largest_ptr = {nullptr};
+	largest_ptr = largest(&i1, &i2);
+	cout << "Returning a pointer from a function: " << *largest_ptr << endl;
+
+	int *my_arr;
+	my_arr = make_array(10);
+	//use it, then
+	delete [] my_arr;
+
+	int *test_ptr {nullptr};
+	//delete test_ptr; //what happens? nothing
+	//what about this instead?:
+	//cout << "dereferenced null pointer: " << *test_ptr << endl; // = Segmentation fault (core dumped)
+
+	//References - like a pointer, but different: must be initialized at creation, and cannot be null, like a constant pointer that is automatically dereferenced
+	vector<string> test {"1","2","3"};
+	for (auto str : test) str = "4";
+	for (auto str : test) cout << str << " ";
+	cout << endl; //no change
+	for (auto &str : test) str = "4";
+	for (auto str : test) cout << str << " ";
+	cout << endl; //all changed
+	//for (auto const &str : test) str = "4"; //<- compiler error; this is good if only displaying the contents without incurring the cost of copying each element into a temp variable
+	//references must be assigned to l-values, cannot be assigned to r-values
+	//int &ref = 100; //compiler error: cannot bind non-const lvalue reference of type ‘int&’ to an rvalue of type ‘int’
+	//same if function f is expecting a reference- cannot pass in an r-value
+ 
+ */
+
 }
 
 /* 
@@ -658,5 +722,29 @@ double a_penny_doubled_everyday(int days, double total_amount) {
     total_amount += a_penny_doubled_everyday(--days, total_amount);
     return total_amount;
  }
+
+ //passing a pointer to a function
+void double_target(int *ptr){
+	*ptr *= 2;
+	//no need to return anything
+}
+
+//returning a pointer from a function
+int *largest(int *ptr1, int *ptr2){
+	if (*ptr1 > *ptr2) return ptr1;
+	else return ptr2;
+}
+
+//returning dynamically allocated memory from the heap
+int *make_array(size_t size, int initial_value){
+	int *new_arr {nullptr};
+	new_arr = new int[size];
+	for (size_t i = 0; i < size; i++){
+		*(new_arr + i) = initial_value;
+		//or use subscript notation like this:
+		//new_arr[i] = initial_value;
+	}
+	return new_arr;
+}
 */
 
