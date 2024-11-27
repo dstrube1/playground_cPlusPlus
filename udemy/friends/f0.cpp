02496318052e8cf6f13f4456000683d81d34fa90
David Strube <dstrube3@gatech.edu>
2024-11-27 08:05:18 -0500

    Update f0.cpp

--- udemy/friends/f0.cpp
+++ udemy/friends/f0.cpp
@@ -3,15 +3,20 @@
 
 using namespace std;
 
-/*
-	private:
-		int priv0;
-		string name {"initial value"};
-*/
-
-/*static*/ int f0::static_num {1}; //TODO: can this be modified by the friend class f1?
+/*static*/ int f0::static_num = 1; 
 
 f0::f0 (){
+	name = "f0";
 	priv0 = static_num + 1;
-	cout << "F0 contructor with no params" << endl;
+	cout << "F0 contructor with no params; priv0 = " << priv0 << endl;
+}
+
+int f0::getPriv0(){ 
+	cout << __func__ << ": ";
+	return priv0; 
+}
+
+string f0::getName() { 
+	cout << __func__ << ": ";
+	return name; 
 }
