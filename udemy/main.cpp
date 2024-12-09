2612128e06411b85f38545504106eaa9aac95c36
David Strube <dstrube3@gatech.edu>
2024-12-08 20:04:49 -0500

    Update main.cpp

--- udemy/main.cpp
+++ udemy/main.cpp
@@ -38,39 +38,7 @@ int main()
 }
 
 /* 
-//TODO: revisit lesson 148 at 4:21
-Shallow::Shallow(const Shallow &source) : data(source.data) {
-	cout << "Copy constructor - shallow" << endl;
-	//Shallow copy - only the pointer is copied - not what it is pointing to.
-	//source and the newly created object both point to the same data area.
-	//When a copied pointer goes out of scope, it is destroyed.
-	//When the copy pointer is destroyed, the source pointer is no longer valid
-}
-
-int main(){
-	Shallow obj{100};
-	display_shallow(obj); //crash here?
-	// obj's data has been released
-	obj.set_data(1000); //or here?
-	Shallow obj0 {obj}; //or here?
-	cout << "Still alive?" << endl;
-	return 0;
-}
-
-//TODO: revisit lesson 149 at 1:37
-Deep::Deep(const Deep &source){
-	data = new int;
-	*data = *source.data;
-	cout << "Copy constructor - deep" << endl;
-}
-
-//Deep copy constructor - delegating constructor
-Deep:Deep(const Deep &source) : Deep{*source.data} {
-	cout << "Copy constructor - deep & delegated" << endl;
-}
-
-//TODO: make folder "constructors" to explore lessons 148-150
-Also, in lesson 150, around 14:50 - noexcept? Was this explained, and if so when?
+In lesson 150, around 14:50 - noexcept? Was this explained, and if so when?
 https://en.cppreference.com/w/cpp/language/noexcept
 https://stackoverflow.com/questions/10787766/when-should-i-really-use-noexcept
 */
