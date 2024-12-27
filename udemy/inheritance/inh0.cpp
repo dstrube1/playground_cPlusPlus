f97aac9d77a1e16f84d5c29a6e28275109536dc4
David Strube <dstrube3@gatech.edu>
2024-12-27 00:21:01 -0500

    Update inh0.cpp

--- udemy/inheritance/inh0.cpp
+++ udemy/inheritance/inh0.cpp
@@ -3,11 +3,23 @@
 
 using namespace std;
 
-inh0::inh0 (){
-	name = "inh0";
+inh0::inh0 (): name {"inh0"}, protectedString {"protectedString-inh0"} {
 	cout << "inh0 contructor with no params" << endl;
+}
+
+inh0::inh0(string s) : name{s}, protectedString {"protectedString-inh0"}  {
+	cout << "inh0 contructor with 1 param" << endl;
 }
 
 string inh0::getName(){
 	return name;
 }
+
+string inh0::getProtectedString(){
+	return protectedString;
+}
+
+inh0::~inh0 (){
+	cout << "inh0 destructor" << endl;
+}
+
