34d49afcaccc97fd20926788525f46efbc1e3f2f
David Strube <dstrube1@users.noreply.github.com>
2024-12-30 15:50:06 -0500

    Update inh1.cpp
    Exploring passing parameter to base constructor

--- udemy/inheritance/inh1.cpp
+++ udemy/inheritance/inh1.cpp
@@ -5,14 +5,16 @@ using namespace std;
 
 inh1::inh1 () 
 : i {0}, protectedString{"fails to compile?"}, protectedInt {1} 
-{ //Note, name was declared as private in inh0, not inh1; so not accessible here
+{ //Note, name was declared as private in inh0, not inh1; so name is not accessible here
 	cout << "inh1 contructor with no params" << endl;
 }
 
 inh1::inh1 (string s) 
-: inh0(s), i {0}, protectedString{"fails to compile?"}, protectedInt {1} 
-{ //Note, name was declared as private in inh0, not inh1; so not accessible here
-	cout << "inh1 contructor with no params" << endl;
+: inh0{s}, protectedString{s}, i {0}, protectedString{"fails to compile?"}, protectedInt {1} 
+{ //Note, name was declared as private in inh0, not inh1; so name is not accessible here, 
+//except as passed along to base constructor
+	//Note also, it seems that the s must both be passed to the base constructor AND to inh1's protectedString member
+	cout << "inh1 contructor with one param:" << s << endl;
 }
 
 void inh1::display(){
