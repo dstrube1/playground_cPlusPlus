0657645543d53a6929c498d880b0ecf51cc31695
David Strube <dstrube3@gatech.edu>
2025-01-02 08:15:05 -0500

    Update inh0.h

--- udemy/inheritance/inh0.h
+++ udemy/inheritance/inh0.h
@@ -10,7 +10,7 @@ using namespace std;
 class inh0{
 	private:
 		string name; 
-		~inh0(); //can the destructor be private?
+		//~inh0(); //destructor can be private; but if it's private in base, then must be private in derived
 	protected:
 		string protectedString;
 	public:
@@ -18,6 +18,7 @@ class inh0{
 		inh0(string);
 		string getName();
 		string getProtectedString();
+		~inh0(); 
 };
 
 #endif //__INH0_H__
