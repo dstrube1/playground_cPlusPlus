bc41a064b2babf9a0388fa49956161f7fb185a5c
David Strube <dstrube3@gatech.edu>
2025-01-15 13:29:44 -0500

    Update inh1.h

--- udemy/inheritance/inh1.h
+++ udemy/inheritance/inh1.h
@@ -7,6 +7,8 @@
 using namespace std;
 
 /*
+see also https://gbg.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535664#overview
+176. Copy/Move Constructors and Operator = with Derived Classes
 */
 
 class inh1: /*access specifier:*/ /**/public/**/ /** /protected/ **/ /** /private/ **/ inh0 {
@@ -18,57 +20,29 @@ class inh1: /*access specifier:*/ /**/public/**/ /** /protected/ **/ /** /privat
 	private:
 		int i; 
 	protected:
-		string protectedString; //Seems like this should fail on compile, 
-		//since protectedString was already declared in inh0
-		int protectedInt; //TODO: accessible from main?
+		string protectedString; //At first, it seems like this should fail on compile, 
+		//since protectedString was already declared in inh0;
+		//but no, it compiles because this (inh1's) protectedString is different from inh0's protectedString
+		int protectedInt; //not accessible from main
 	public:
 		inh1(); //constructors are not inherited from base class by derived class
 		//Therefore, if both base and derived have a constructor with a param, but only base implements it,
-		//then compile will fail. TODO - test this; 
+		//then compile will fail.
 		//also if there is a base constructor with a param, it must be called from derived constructor with a param
 		inh1(string);
 		void display();
-		~inh1();
+		string getName();
+		string getProtectedString();
+		virtual void virtualMethod() const override;
+		virtual ~inh1();
 		//When a class derives from a parent, constructor / destructor order is this:
 		// Base constructor, derived constructor, derived destructor, base destructor
 /*
-inh: see also https://gbg.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535664#overview
-176. Copy/Move Constructors and Operator = with Derived Classes
 
-redefine method from base class:
-inh1::getName(){
-	cout << "inh1's getName calls inh0's getName\n";
-	return inh0::getName();
-}
+overloaded functions and overloaded operators are compile-time polymorphism 
+(aka, early-binding polymorphism and static binding polymorphism), as opposed to run-time polymorphism
 
-also: 
-//static binding example
-inh0 *inh0_ptr = new inh1();
-inh0_ptr->getName(); //TODO: Confirm: calls inh0 getName, not inh1 ?
-delete *inh0_ptr;
-
-also:
-overloaded functions and overloaded operators are compile-time polymorphism (aka, early-binding polymorphism and static binding polymorphism), as opposed to run-time polymorphism
 run-time polymorphism requires inheritance
-try this, in main:
-void call_from_reference(inh0 &i0){
-	i0.getProtectedString();
-}
-implement getProtectedString() in ih1, then :
-inh0 i0 = new inh1();
-i0.getProtectedString();
-and 
-inh1 i1 = new inh1();
-i1.getProtectedString();
-see what happens
-
-then make new method in inh0: public virtual void test();
-and declared and implemented in inh1: public void test();
-
-smart pointer:
-std::unique_ptr<inh0> ptr = std::make_unique<inh1>();
-ptr->test();
-//don't have to delete smart pointers
 
 */
 };
