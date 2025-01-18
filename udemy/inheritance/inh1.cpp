cf8965d5d4290193c41436e237e2d401765ef69e
David Strube <dstrube3@gatech.edu>
2025-01-18 06:25:07 -0500

    Update inh1.cpp

--- udemy/inheritance/inh1.cpp
+++ udemy/inheritance/inh1.cpp
@@ -1,5 +1,6 @@
 #include "inh1.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -9,17 +10,36 @@ inh1::inh1 ()
 	cout << "inh1 contructor with no params" << endl;
 }
 
-inh1::inh1 (string s) 
+/**/inh1::inh1 (string s) 
 : inh0{s}, protectedString{s}, i {0}, protectedInt {1} 
 { //Note, name was declared as private in inh0, not inh1; so name is not accessible here, 
 //except as passed along to base constructor
 	//Note also, it seems that the s must both be passed to the base constructor AND to inh1's protectedString member
 	cout << "inh1 contructor with one param:" << s << endl;
-}
+}/**/
 
-void inh1::display(){
+//'final' specifier is not allowed outside a class definition
+void inh1::display() const /*final*/{
 	cout << "protectedString: " << protectedString;
 	cout << "; protectedInt: " << protectedInt << "; i: " << i << endl;
+}
+
+//redefine method from base class
+string inh1::getName(){
+	cout << "inh1's getName calls inh0's getName by redefining it: ";
+	return inh0::getName();
+}
+
+string inh1::getProtectedString(){
+	protectedString = "protectedString from inh1";
+	cout << "getProtectedString from inh1: ";
+	return protectedString;
+}
+
+//'virtual' can only be specified inside the class definition
+//'override' specifier is not allowed outside a class definition
+/*virtual*/ void inh1::virtualMethod() const /*override*/{
+	cout << "virtualMethod from inh1\n";
 }
 
 inh1::~inh1 (){
