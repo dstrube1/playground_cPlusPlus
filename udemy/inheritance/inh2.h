29a0d26bb242d01e6bae1603c3b6a72253233631
David Strube <dstrube1@users.noreply.github.com>
2025-01-21 10:07:37 -0500

    Update inh2.h

--- udemy/inheritance/inh2.h
+++ udemy/inheritance/inh2.h
@@ -15,3 +15,13 @@ class inh2 final: public inh1 {
 };
 
 #endif //__INH2_H__
+/*
+//interface example:
+class Printable{
+	friend ostream &operator<<(ostream &, const Printable &obj);
+	public:
+		virtual void print(ostream &os) const = 0;
+		virtual Printable(){/*not sure if this is permissible:* / cout "Printable destructor\n";};
+};
+
+*/
