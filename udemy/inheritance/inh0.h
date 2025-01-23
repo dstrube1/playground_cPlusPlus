d9e7e37b0e362587f0443e06909a384bbb49135c
David Strube <dstrube3@gatech.edu>
2025-01-23 11:19:13 -0500

    Update inh0.h

--- udemy/inheritance/inh0.h
+++ udemy/inheritance/inh0.h
@@ -16,9 +16,37 @@ class inh0{
 	public:
 		inh0();
 		inh0(string);
+		virtual void display() const;
 		string getName();
 		string getProtectedString();
-		~inh0(); 
+		virtual void virtualMethod() const;
+		virtual ~inh0(); 
 };
 
 #endif //__INH0_H__
+/*
+TODO: 
+
+using base class references:
+inh1 i1_origin;
+inh0 &i1_ref = i1_origin;
+i1_ref.virtualMethod();
+
+abstract class: if a class has at least 1 pure virtual function, or inherits from another abstract class and itself has no non-virtual functions*
+new abstract base class inh, from which inh0 inherits ?
+virtual void virtualMethod() = 0; // pure virtual function
+virtual ~inh() {cout << "inh destructor\n";}
+inh i; //can't instantiate inh like this
+inh *i_ptr = new ihn(); //or this
+//but can do this:
+//inh *i_ptr = new inh0();
+*: test this: what if inh4 derives from inh, and has no non-automatic (constructor / desctructor) method? can I create it like this?
+inh4 i4;
+//to pass a vector of pointers as a reference (so as to not copy anything):
+void doStuff(vector<inh *> &vec){
+	for(const auto i : vec){
+		i.virtualMethod();
+	}
+}
+
+*/
