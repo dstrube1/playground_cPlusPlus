a5bb83dd65fed9705c43b5429edd309800371f5f
David Strube <dstrube3@gatech.edu>
2025-01-26 19:58:36 -0500

    Update main.cpp

--- udemy/inheritance/main.cpp
+++ udemy/inheritance/main.cpp
@@ -2,14 +2,14 @@
 #include <string>
 #include "inh0.h"
 #include "inh1.h"
+#include "inh2.h"
+#include <vector>
 
 using namespace std;
 
 /*
 # To compile:
-g++ -std=c++14 -o main.o main.cpp inh0.cpp
-g++ -o main.o main.cpp inh0.cpp inh1.cpp
-g++ -o main.o main.cpp inh0.cpp inh1.cpp inh2.cpp
+g++ -std=c++14 -o main.o main.cpp inh0.cpp inh1.cpp inh2.cpp
 
 # without -o, outputs to default a.out
 # without -std=c++14 , {} initializers fail
@@ -24,7 +24,7 @@ Multiple inheritance: new class created from two or more classes
 
 inh0: Base / super / parent class
 inh1: Derived from inh0 - child / sub class
-TODO: inh2: Derived from inh0 and inh1
+TODO: inh2: Derived from inh0 and inh1?
 
 See also: UML class diagrams
 
@@ -35,11 +35,14 @@ Specialization: new classes with specialized attributes
 
 */
 
+void call_from_reference(inh0 &i0);
+
 //////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////BEGIN main
 //////////////////////////////////////////////////////////////////////////////////////////
 int main()
 {
+/**/
 	inh0 i0a;
 	inh0 i0b("i0b");
 	
@@ -49,12 +52,59 @@ int main()
 	inh1 i1a;
 	inh1 i1b("i1b");
 	
+	//Not allowed:
+	//i1a.protectedInt = 1;
+	
+	//Note, if not declared and implemented in ih1, getName is still callable from i1a
 	cout << "i1a.getName(): " << i1a.getName() << endl;
 	
 	cout << "i1a.getProtectedString(): " << i1a.getProtectedString() << endl;
 	
 	cout << "i1b.display(): ";
 	i1b.display();
+
+	//static binding example
+	cout << "static binding example: \n";
+	inh0 *inh0_ptr = new inh1();
+	//The following calls inh0 getName, not inh1's:
+	cout << "Even though inh0_ptr is assigned to a new inh1, it calls inh0's getName:\n";
+	cout << "inh0_ptr getName(): " << inh0_ptr->getName() << endl; 
+	//Good practice to always delete pointers when you're done with them
+	delete inh0_ptr;
+
+	//run-time polymorphism example
+	cout << "run-time polymorphism example: \n";
+	inh1 i1c;
+	call_from_reference(i1c);
+/**/
+	//another example of run-time polymorphism, using smart pointer:
+	cout << "run-time polymorphism via smart pointer example: \n";
+	std::unique_ptr<inh0> smart_ptr = std::make_unique<inh1>();
+	smart_ptr->virtualMethod();
+	//We don't have to delete smart pointers.
+	//Note, this automatically calls the inh0 destructor, 
+	//but interestingly doesn't seem to call the inh1 destructor
+	
+	cout << "run-time polymorphism via array example: \n";
+	inh0 *i0 = new inh0();
+	inh0 *i1 = new inh1();
+	inh0 *i2 = new inh2();
+	inh0 *arr [] = {i0, i1, i2};
+	for (auto i=0; i < 3; ++i){
+		arr[i]->virtualMethod();
+	}
+
+	cout << "run-time polymorphism via vector example: \n";
+	vector<inh0 *> vec {i0, i1, i2};
+	for (auto vec_ptr : vec){
+		vec_ptr->virtualMethod();
+	}
+	cout << "deleting inh0 pointer to inh0\n";
+	delete i0;
+	cout << "deleting inh0 pointer to inh1\n";
+	delete i1;
+	cout << "deleting inh0 pointer to inh2\n";
+	delete i2;
 
 	cout << "Done" << endl;
 	return 0;
@@ -63,6 +113,15 @@ int main()
 ///////////////////////////END main
 //////////////////////////////////////////////////////////////////////////////////////////
 
+void call_from_reference(inh0 &i0){
+	cout << "inh0 reference's call to getProtectedString() (where getProtectedString is not a virtual method): " << i0.getProtectedString()<< endl;
+	
+	//Example of run-time polymorphism, as opposed to compile-time polymorphism
+	cout << "inh0 reference's call to virtualMethod() (where the inh0 is actually an inh1): "<< endl;
+	i0.virtualMethod();
+	
+	//Shouldn't matter whether the virtual method is void or actually returns something
+}
 
 
 
