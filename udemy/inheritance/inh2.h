7753551b119506417581f7288e0b810a1f6cb204
David Strube <dstrube3@gatech.edu>
2025-02-01 12:45:32 -0500

    Update inh2.h

--- udemy/inheritance/inh2.h
+++ udemy/inheritance/inh2.h
@@ -15,22 +15,6 @@ class inh2 final: public inh1 {
 		virtual ~inh2();
 };
 
-
-/**/
-//1- Is it okay to define two classes in 1 .h file?
-//2- interface example:
-class Printable{
-	friend ostream &operator<<(ostream &, const Printable &obj);
-	public:
-		virtual void print(ostream &os) const = 0; //the "= 0" is what makes this a pure virtual function
-		//An abstract class cannot be instantiated and has only pure virtual functions
-		
-		//constructor cannot be virtual 
-		//Printable(){cout << "Printable destructor\n";}
-		
-		//TODO: What if a class has a pure virtual function AND a constructor? Can it be instantiated?
-};
-
 /**/
 
 #endif //__INH2_H__
