58f8ce3220ad69d42a4718c5cde82131ee58b4db
David Strube <dstrube3@gatech.edu>
2025-02-07 09:47:21 -0500

    Update PrintMe.cpp

--- udemy/inheritance/PrintMe.cpp
+++ udemy/inheritance/PrintMe.cpp
@@ -8,6 +8,10 @@ PrintMe::PrintMe(){
 }
 
 void PrintMe::print(ostream &os) const {
+	//to set precision to 2 decimal places:
+	//os.precision(2);
+	//to set fixed point:
+	//os << std::fixed;
 	os << "print from PrintMe" << endl;
 }
 
