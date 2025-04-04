ff82a0dc6b835b24fe65cb576bfb27b84edfb80f
David Strube <dstrube3@gatech.edu>
2025-04-04 02:42:50 -0400

    Update L11.cpp

--- udemy/L11.cpp
+++ udemy/L11.cpp
@@ -3,6 +3,10 @@
 #include <algorithm>
 #include <numeric> //required for accumulate
 #include <utility> //required for std::pair
+#include <list>
+#include <map>
+#include <set>
+#include <cctype> //for ::toupper
 
 using namespace std;
 
@@ -76,6 +80,7 @@ void typeTest(T1 a, T2 b){
 }
 
 void classTemplateExamples();
+
 /*
 Make this class generic so that value can be an int or double
 Can't overload classes with the same name in the same namespace
@@ -91,6 +96,7 @@ class Item{
 		string get_name() const {return name;}
 };
 */
+
 template <typename T> 
 class Item{
 	//Note, template classes are usually completely contained in header (.h) files, no .cpp
@@ -112,17 +118,48 @@ struct TemplatedStruct {
 	T2 second; //can be the same or different type
 };
 
+template <typename T, int N> 
+class GATC{ //Generic Array Template Class
+	//initialized like this:
+	//GATC<N> gatc;, where N is the size, not the type; this will be a non-type templated class
+	//This is just for an example; if really need something like this, use std::array
+	int size {N};
+	T values[N];
+	friend ostream &operator<<(ostream &os, const GATC<T, N> &arr){
+		os << "[";
+		for(const auto &val : arr.values){
+			os << val << " ";
+		}
+		os << "]" << endl;
+		return os;
+	}
+	public:
+		GATC() = default;
+		GATC(T init_val){
+			for(auto &item : values) item = init_val;
+		}
+		void fill(T val){ for(auto &item : values) item = val; }
+		int get_size() const { return size; }
+		//overloaded subscript operator
+		T &operator[](int index){ //is 'index' a reserved keyword?
+			return values[index];
+			//Usage: nums[i] 
+			//which is(?) equivalent to: nums.operator[](i)
+		}
+};
+
+void containersExamples();
 
 //////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////BEGIN main
 //////////////////////////////////////////////////////////////////////////////////////////
 int main()
 {
-	//algorithmExamples();
+	algorithmExamples();
 	//macroExamples();
 	//functionTemplateExamples();
-	
-	classTemplateExamples();
+	//classTemplateExamples();
+	//containersExamples();
 	
 	cout << "\nDone\n\n";
 	return 0;
@@ -131,37 +168,125 @@ int main()
 ///////////////////////////END main
 //////////////////////////////////////////////////////////////////////////////////////////
 
+struct Square_Functor {
+	void operator()(int x){ //overload () operator
+		cout << x * x << " ";
+	}
+};
+
+void square_function_pointer(int x){
+	cout << x * x << " ";
+}
+
 void algorithmExamples(){
 	//Simple example:
-	vector<int> v {1,5,3};
+	vector<int> vec {1,5,3};
 	cout << "vector before sorting:\n";
-	for(auto element : v){
+	for(auto element : vec){
 		cout << element << " ";
 	}
 	cout << endl;
 	
 	//Sort from beginning to end:
-	sort(v.begin(), v.end());
+	sort(vec.begin(), vec.end());
 	//Sort works on not just vectors
 	//Also, can specify how to sort, like sort account objects by balance
 	
 	cout << "vector after sorting:\n";
-	for(auto element : v){
+	for(auto element : vec){
 		cout << element << " ";
 	}
 	cout << endl;
 	
 	//Reverse
-	reverse(v.begin(), v.end());
+	reverse(vec.begin(), vec.end());
 	cout << "vector after reversing:\n";
-	for(auto element : v){
+	for(auto element : vec){
 		cout << element << " ";
 	}
 	cout << endl;
 	
 	//Accumulate - requires #include <numeric>
-	int sum = std::accumulate(v.begin(), v.end(), 0);
+	int sum = std::accumulate(vec.begin(), vec.end(), 0);
 	cout << "sum of vector:\n" << sum << endl;		
+	
+	//More:
+	//https://en.cppreference.com/w/cpp/algorithm
+	
+	//All STL algorithms expect at least one iterator
+	
+	//The possibility of iterator invalidation is something to be aware of. For example:
+	//while iterating over a collection, call clear(). Then what? 
+	//Undefined; iterator will be unknowingly invalid, so be careful.
+	
+	//Find
+	auto loc = find(vec.begin(), vec.end(), 3);
+	if (loc != vec.end()) {//found it!
+		cout << "found " << *loc << endl; 
+		//find returns iterator pointing to the first found instance...
+		//but where? loc is not the index, it's a pointer to the value
+	}
+	//This works for any container of objects that have '==' defined.
+	//If using find on a container of custom objects of class C, then C must override operator==
+	
+	//for_each - applies function to each element
+	//function must be provided as:
+	//	functor (function object),
+	//	function pointer,
+	//	or lambda 
+	Square_Functor square_functor;
+	cout << "Squaring with a functor (aka function object): ";
+	for_each(vec.begin(), vec.end(), square_functor);
+	cout << endl;
+	
+	cout << "Squaring with a function pointer: ";
+	for_each(vec.begin(), vec.end(), square_function_pointer);
+	cout << endl;
+
+	//Lambda - introduced in C++11, aka closure, block, and anonymous function
+	cout << "Squaring with a lambda: ";
+	for_each(vec.begin(), vec.end(), 
+		[](int x) {cout << x * x << " ";}  //lambda
+		);
+	cout << endl;
+
+	cout << "Original vector is unchanged: ";
+	for(auto element : vec){
+		cout << element << " ";
+	}
+	cout << endl;
+	
+	//Count
+	int lookFor = 3;
+	int num = count(begin(vec), end(vec), lookFor);
+	cout << "Found '" << lookFor << "' " << num << " time(s)\n";
+	lookFor = 4;
+	num = count(begin(vec), end(vec), lookFor);
+	cout << "Found '" << lookFor << "' " << num << " time(s)\n";
+	
+	//Count if (if some condition is met), using a lambda
+	int num_evens = count_if(begin(vec), end(vec), [](int x){return x % 2 == 0;});
+	int num_odds = count_if(begin(vec), end(vec), [](int x){return x % 2 != 0;});
+	cout << "Found " << num_evens << " evens and " << num_odds << " odds\n";
+	
+	//Replace - affects container in place, replaces all found
+	vec.push_back(1);
+	replace(begin(vec), end(vec), 1, 2);
+	cout << "Vector with 1 added, then replace 1 with 2: ";
+	for(auto element : vec){
+		cout << element << " ";
+	}
+	cout << endl;
+	
+	//all_of: returns true if the lambda function returns true for all elements
+	bool allLessThan100 = all_of(begin(vec), end(vec), [](int x){return x < 100;});
+
+	//transform - apply passed in function to all elements
+	//3rd param is where to start the transformation
+	string s = "this is a test";
+	cout << "Before transform: " << s << endl;
+	transform(begin(s), end(s), begin(s), ::toupper);
+	cout << "After transform: " << s << endl;
 }
 
 void macroExamples(){
@@ -287,4 +412,100 @@ void classTemplateExamples(){
 	cout << "Instance of Item with templated datatype Item<int>: name: " << item0.get_name() << endl;
 	cout << "\t\t\t\t\t  ^ value's name: " << item0.get_value().get_name() << endl;
 
+	//Generic Array Template Class
+	GATC<int, 5> gatci; //elements will be type int, size is 5
+	cout << "Size of gatc of ints is " << gatci.get_size() << endl;
+	cout << "Initial junk contents are: " << gatci << endl;
+	GATC<float, 5> gatcf; 
+	cout << "Initial junk contents of gatc of floats are " << gatcf << endl;
+}
+
+void containersExamples(){
+	/*Member functions common to all containers:
+	default constructor
+	overloaded constructor
+	copy constructor
+	move constructor
+	destructor
+	copy assignment
+	move assignment 
+	size
+	empty
+	insert
+	< <=
+	> >=
+	== !=
+	swap
+	erase
+	clear
+	begin end - iterators
+	rbegin rend - reverse_iterator
+	cbegin cend - const_iterator
+	crbegin crend - const_reverse_iterator
+	
+	Containers must contain elements that have or overload < & ==
+	
+	Iterators work like pointers.
+	Most container classes be be traversed by iterators except stack & queue
+	*/
+	
+	//Iterator examples:
+	vector<int>::iterator itV;
+	list<string>::iterator itL;
+	map<string, string>::iterator itM;
+	set<char>::iterator itS;
+	
+	//vector is implemented in contiguous memory
+	vector<int> vec {1,2,3};
+	//point to first element:
+	itV = vec.begin();
+	//point to one after last element
+	itV = vec.end();
+	
+	set<char> abc {'a','b','c'};
+	//set may or may not be implemented in contiguous memory
+	itS = abc.begin();
+	
+	//if container is empty, then , e.g., vec.begin() will point to vec.end()
+	
+	//easier way to initialize an iterator:
+	auto itVb = vec.begin();
+	//alternatively
+	//auto itVb = std::begin(vec);
+	
+	//See L11.png for table of common operations with iterators
+	
+	//Common traversal:
+	cout << "Traversing vector with iterator: ";
+	while(itVb != itV) {//begin != end
+		cout << *itVb << " ";
+		itVb++;
+	} //this is what range based for loop over a container does behind the scenes
+	cout << endl;
+
+	//While vector can randomly access elements, some containers cannot, 
+	//thus sometimes iterators are necessary
+	//Same as above but more readable:
+	///for(auto it = vec.begin(); it != vec.end(); it++){ ... }
+	
+	//Reverse iterator: works in reverse;
+	//last is first and first is last
+	//++ moves backwards, -- moves forward
+	vector<int>::reverse_iterator itVr = vec.rbegin();
+	cout << "Traversing vector with reverse iterator: ";
+	while(itVr != vec.rend()) {
+		cout << *itVr << " ";
+		itVr++;
+	} 
+	cout << endl;
+	
+	//Constant iterator (read only values from the container) can be declared like this:
+	vector<int>::const_iterator itVc0 = vec.begin();
+	// or this:
+	auto itVc1 = vec.cbegin();
+	
+	//Map example
+	map<string, string> stuff {{"a","1"},{"b","2"}};
+	itM = stuff.begin();
+	cout << "An element in a map: {" << itM->first << "," << itM->second << "}\n";
 }
